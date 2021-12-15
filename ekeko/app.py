ddc0b4f97cc1db18676ed957ca09a6b5fad32206
otreblan <otreblain@gmail.com>
2021-12-14 21:33:27 -0500

    Add crud functions

--- ekeko/app.py
+++ ekeko/app.py
@@ -5,5 +5,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/user")
+def user():
+    pass
+
+@app.route("/order")
+def order():
+    pass
+
+@app.route("/market")
+def market():
+    pass
+
 def main() -> None:
     app.run(port=int(getenv("PORT", 5000)), host=("0.0.0.0"))
